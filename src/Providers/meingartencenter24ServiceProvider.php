b481ecb0a858d6e9eeb8f9515ba665f1b7369a0d
Daniel Kaschlaw <d.kaschlaw@zentral-kommunikation.de>
2018-02-09 09:57:36 +0100

    First Push Header

--- src/Providers/meingartencenter24ServiceProvider.php
+++ src/Providers/meingartencenter24ServiceProvider.php
@@ -25,15 +25,27 @@ class meingartencenter24ServiceProvider extends ServiceProvider
 
     public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
     {
-        $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) { 
-            if($componentContainer->getOriginComponentTemplate() == 'Ceres::Customer.Components.LoginView') {
-                    $componentContainer->setNewComponentTemplate('meingartencenter24::Customer.Components.LoginView');
-            } 
-        }, self::EVENT_LISTENER_PRIORITY);
+        //twig replacement
+        // provide template to use for homepage
+        // $eventDispatcher->listen('IO.tpl.home', function(TemplateContainer $container, $templateData) {
+        //     $container->setTemplate("Kiom::Homepage.Homepage");
+        //     return false;
+        // });
 
-        $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) { 
-            if($componentContainer->getOriginComponentTemplate() == 'Ceres::Customer.Components.GuestLogin') {
-                    $componentContainer->setNewComponentTemplate('meingartencenter24::Customer.Components.GuestLogin');
+
+        //partial replacement
+        $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
+            $partial->set('header', 'meingartencenter24::PageDesign.Partials.Header.Header');            
+            $partial->set('page-design', 'meingartencenter24::PageDesign.PageDesign');
+
+        }, self::EVENT_LISTENER_PRIORITY);
+        
+
+        //vue components replacement
+        $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) {         
+            if($componentContainer->getOriginComponentTemplate() == 'Ceres::Customer.Components.UserLoginHandler') 
+            {
+                $componentContainer->setNewComponentTemplate('meingartencenter24::Customer.Components.UserLoginHandler');
             } 
         }, self::EVENT_LISTENER_PRIORITY);
 
