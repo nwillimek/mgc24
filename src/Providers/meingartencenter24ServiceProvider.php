23b415b7d41910d74fcd30c994ac28fb89431af1
Daniel Kaschlaw <d.kaschlaw@zentral-kommunikation.de>
2018-02-15 14:32:16 +0100

    Adding SP for SingleItemView

--- src/Providers/meingartencenter24ServiceProvider.php
+++ src/Providers/meingartencenter24ServiceProvider.php
@@ -42,16 +42,18 @@ class meingartencenter24ServiceProvider extends ServiceProvider
         
 
         //vue components replacement
-        $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) {         
+        
+        $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) { 
             if($componentContainer->getOriginComponentTemplate() == 'Ceres::Customer.Components.UserLoginHandler') 
             {
                 $componentContainer->setNewComponentTemplate('meingartencenter24::Customer.Components.UserLoginHandler');
             } 
-        }, self::EVENT_LISTENER_PRIORITY);
-
-        $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) { 
             if($componentContainer->getOriginComponentTemplate() == 'Ceres::ItemList.Components.CategoryItem') {
                     $componentContainer->setNewComponentTemplate('meingartencenter24::ItemList.Components.CategoryItem');
+            }
+            if($componentContainer->getOriginComponentTemplate() == 'Ceres::Item.Components.SingleItem') 
+            {
+                $componentContainer->setNewComponentTemplate('meingartencenter24::Item.Components.SingleItem');
             } 
         }, self::EVENT_LISTENER_PRIORITY);
 
