cf48c99288415fff1f7b07e8e052d43b7f0375ed
Daniel Kaschlaw <d.kaschlaw@zentral-kommunikation.de>
2018-02-20 17:35:54 +0100

    Avialability Preview

--- src/Providers/meingartencenter24ServiceProvider.php
+++ src/Providers/meingartencenter24ServiceProvider.php
@@ -48,10 +48,6 @@ class meingartencenter24ServiceProvider extends ServiceProvider
         //vue components replacement
         
         $eventDispatcher->listen('IO.Component.Import', function(ComponentContainer $componentContainer) { 
-            if($componentContainer->getOriginComponentTemplate() == 'Ceres::Basket.Components.AddToBasket') 
-            {
-                $componentContainer->setNewComponentTemplate('meingartencenter24::Basket.Components.AddToBasket');
-            }
             if($componentContainer->getOriginComponentTemplate() == 'Ceres::Customer.Components.UserLoginHandler') 
             {
                 $componentContainer->setNewComponentTemplate('meingartencenter24::Customer.Components.UserLoginHandler');
