36702425063837391bf667bb1f1aa0e3d4f4ac42
Luis Nunes <dnun3s@gmail.com>
2018-03-15 10:47:38 +0100

    UPDATE adding checkout.confirmation template

--- src/Providers/meingartencenter24ServiceProvider.php
+++ src/Providers/meingartencenter24ServiceProvider.php
@@ -36,12 +36,17 @@ class meingartencenter24ServiceProvider extends ServiceProvider
             return false;
         });
 
+        // provide template to use for confirmation
+        $eventDispatcher->listen('IO.tpl.confirmation', function(TemplateContainer $container, $templateData) {
+            $container->setTemplate("meingartencenter24::Checkout.OrderConfirmation");
+            return false;
+        });
+
         //partial replacement
         $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
             $partial->set('head', 'meingartencenter24::PageDesign.Partials.Head');
             $partial->set('header', 'meingartencenter24::PageDesign.Partials.Header.Header');
             $partial->set('footer', 'meingartencenter24::PageDesign.Partials.Footer');
-            $partial->set('page-design', 'meingartencenter24::PageDesign.PageDesign');
 
         }, self::EVENT_LISTENER_PRIORITY);
         
