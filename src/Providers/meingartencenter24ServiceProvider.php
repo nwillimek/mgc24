adec16ca344806034db65f20b42f1fbdabe7843a
Luis Nunes <dnun3s@gmail.com>
2018-03-19 16:31:48 +0100

    UPDATE pageDesign overwrite

--- src/Providers/meingartencenter24ServiceProvider.php
+++ src/Providers/meingartencenter24ServiceProvider.php
@@ -47,6 +47,7 @@ class meingartencenter24ServiceProvider extends ServiceProvider
             //$partial->set('head', 'meingartencenter24::PageDesign.Partials.Head');
             $partial->set('header', 'meingartencenter24::PageDesign.Partials.Header.Header');
             $partial->set('footer', 'meingartencenter24::PageDesign.Partials.Footer');
+            $partial->set('page-design', 'meingartencenter24::PageDesign.PageDesign');
 
         }, self::EVENT_LISTENER_PRIORITY);
         
