0145daaa08dc51382e066ef862a33ab469f06dc4
Luis Nunes <dnun3s@gmail.com>
2018-03-19 16:03:04 +0100

    UPDATE removed scripts

--- src/Providers/meingartencenter24ServiceProvider.php
+++ src/Providers/meingartencenter24ServiceProvider.php
@@ -37,14 +37,14 @@ class meingartencenter24ServiceProvider extends ServiceProvider
         });
 
         // provide template to use for confirmation
-        $eventDispatcher->listen('IO.tpl.confirmation', function(TemplateContainer $container, $templateData) {
-            $container->setTemplate("meingartencenter24::Checkout.OrderConfirmation");
-            return false;
-        });
+        // $eventDispatcher->listen('IO.tpl.confirmation', function(TemplateContainer $container, $templateData) {
+        //     $container->setTemplate("meingartencenter24::Checkout.OrderConfirmation");
+        //     return false;
+        // });
 
         //partial replacement
         $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
-            $partial->set('head', 'meingartencenter24::PageDesign.Partials.Head');
+            //$partial->set('head', 'meingartencenter24::PageDesign.Partials.Head');
             $partial->set('header', 'meingartencenter24::PageDesign.Partials.Header.Header');
             $partial->set('footer', 'meingartencenter24::PageDesign.Partials.Footer');
 
